c1b20b2de02f060ef649c27543fd731bacffe0ff
yassine <yassine.n13@gmail.com>
2024-03-15 20:35:24 +0100

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Test} from "./components/Test.jsx"
+
+function App() {
+
+  return <div>
+    <h1>Hello world !</h1>
+    <Test/>
+  </div>
+}
+
+export default App
---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+export function Test() {
+
+  const [test, setTest] = useState(0)
+
+
+  return <h2>Test component</h2>
+}
