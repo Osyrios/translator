ecb95cf1bb07f0ab5c6abf3b3872bb9892a3d0df
Yassine <yassine.n13@gmail.com>
2024-03-17 18:30:22 +0100

    traduct done

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,8 +1,10 @@
 import {Header} from "../layout/Header.jsx";
 
 export function Todo(){
+
+
     return <div>
         <Header/>
-        bienvenue sur la page de liste de tache
+        <h2></h2>
     </div>
 }
