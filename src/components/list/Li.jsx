eb9ac3475e1ff2bb149b2a3dec3bfe726edcd295
Yassine <yassine.n13@gmail.com>
2024-06-21 11:11:45 +0200

    change navbar behaviour

--- src/components/list/Li.jsx
+++ src/components/list/Li.jsx
@@ -2,6 +2,6 @@ import {NavLink} from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 export function Li({navTo, name}){
-    return <li className="p-3 rounded-3xl hover:bg-gradient-to-b hover:from-green-400 hover:to-gray-300 hover:shadow-lg hover:shadow-gray-800">
-        <NavLink to={navTo}>{name}</NavLink></li>
+    return <li className="flex w-1/4 h-full justify-center items-center hover:bg-gradient-to-b hover:from-green-400 hover:to-gray-300 hover:shadow-lg hover:shadow-gray-800">
+        <NavLink to={navTo} className="flex w-full h-full items-center justify-center">{name}</NavLink></li>
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,8 +2,8 @@ import {Li} from "../components/list/Li.jsx";
 
 export function Header(){
     return <div>
-        <nav className="py-3 mb-2 bg-gradient-to-b from-green-400 to-gray-300 shadow-lg shadow-gray-600">
-            <ul className="flex flex-row justify-evenly font-designer text-xs text-black ">
+        <nav className="mb-2 h-10 bg-gradient-to-b from-green-400 to-gray-300 shadow-lg shadow-gray-600">
+            <ul className="flex h-full flex-row font-designer text-xs text-black ">
                 <Li navTo="/" name="Home" />
                 <Li navTo="/translator" name="Traducteur" />
                 <Li navTo="/todo" name="TODO List" />
