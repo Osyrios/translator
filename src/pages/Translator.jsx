28705206a89fa90c7077180ad759c7fa3db602d5
Yassine <yassine.n13@gmail.com>
2024-09-21 20:03:04 +0200

    finish translator

--- src/pages/Translator.jsx
+++ src/pages/Translator.jsx
@@ -40,16 +40,15 @@ export function Translator() {
   }
 
 
-
   return (
     <div>
       <Header />
-      <h1 className="uppercase">Translation tool</h1>
+      <h1 className="uppercase pt-8 text-center font-bold">Bienvenue sur le traducteur en ligne</h1>
       <div className="font-roboto mt-24 flex flex-col items-center">
 
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center w-full">
           <h3 className="text-center">Saisissez votre texte</h3>
-          <input onChange={handleChangeInputText} className=" text-center border-2 border-violet-500 p-2 rounded-2xl"/>
+          <textarea onChange={handleChangeInputText} className=" text-center border-2 border-violet-500 p-2 rounded-2xl w-3/5"/>
 
           <div className="flex flex-col items-center pt-5 overflow-hidden">
             <label htmlFor="select">Traduire en:</label>
@@ -63,12 +62,12 @@ export function Translator() {
         </div>
 
         <div className="flex flex-row justify-center">
-          <button type="button" onClick={handleTranslation} className="mt-10 p-2 text-center rounded-3xl bg-violet-500 italic font-bold"> Traduire</button>
+          <button type="button" onClick={handleTranslation} className="mt-10 p-1 px-5 text-center rounded-3xl bg-violet-500 italic font-bold"> Traduire</button>
         </div>
 
-        <div className="flex flex-col items-center mt-10 ">
+        <div className='flex flex-col items-center mt-10 w-full h-fill overflow-auto	'>
           <h3>Résultat</h3>
-          <p className="border p-3 m-2 rounded-2xl border-violet-500">{translatedText}</p>
+          <div className="border p-3 m-2 rounded-2xl border-violet-500 w-3/5 ">{translatedText}</div>
         </div>
 
       </div>
